eec7828430bb63d2b08b1eb253a6452c530e02e9
azin.makaranth <azin.makaranth@gmail.com>
2018-02-14 15:18:05 +0530

    spaces

--- answer.php
+++ answer.php
@@ -17,13 +17,13 @@ if (!isset($_SESSION["fbuid"]))
 	<link rel="stylesheet" href="css/style.css" type="text/css">
 	<link rel="stylesheet" type="text/css" href="css/mobile.css">
 	<script src="js/mobile.js" type="text/javascript"></script>
-	
-	
+
+
 	<style>
 		img.ack {
 		width:500px;
 		height:300px;
-		
+
 		}
 		.ack {
 		color:green;
@@ -41,7 +41,7 @@ if (!isset($_SESSION["fbuid"]))
 		<div id="header">
 			<div id="navigation">
 				<span id="mobile-navigation">&nbsp;</span>
-				
+
 				<ul id="menu">
 					<li class="selected">
 						<a href="index.php">HOME</a>
@@ -51,41 +51,43 @@ if (!isset($_SESSION["fbuid"]))
 					</li>
 					<li>
 						<a href="rules.php">RULES</a>
-						
+
 					</li>
 					<li>
-					<a href="https://www.facebook.com/decipher.ritu" target="_blank">ALFRED</a>	
-						
+					<a href="https://www.facebook.com/decipher.ritu" target="_blank">ALFRED</a>
+
 					</li>
 					<?php
-					
+
 			             	session_start();
 					if (isset($_SESSION["fbuid"]))
 					echo "<li>
-						<a href=\"logout.php\">logout</a>					
+						<a href=\"logout.php\">logout</a>
 					</li>";
 					?>
-					
+
 				</ul>
 			</div>
 		</div>
 		<div id="body" class="home">
 			<div class="header">
 				<div>
-					
+
 					<!--php code goes here-->
-<?php 
+<?php
 	require_once("database.php");
 	global $result;
 	$level = $_SESSION["level"];
 	$answer = $_GET["answer"];
+
 	$name=$_SESSION["name"];
 	$sql = "INSERT INTO logs (user,val,level) VALUES ('" .$name. "','" . mysqli_real_escape_string($result,$answer) . "','". $level . "')";
 
 	$ref = $result->query($sql);
 
 	$sql = "SELECT * FROM levels WHERE name = '" . mysqli_real_escape_string($result,$level) . "'" ;
-	
+  $answer = preg_replace('/\s+/', '', $answer);
+  $answer = str_replace(' ', '', $answer);
 	$ref = $result->query($sql);
 	$row = mysqli_fetch_assoc($ref);
 	$answer=strtolower($answer);
@@ -93,7 +95,7 @@ if (!isset($_SESSION["fbuid"]))
 	$ans=strtolower($ans);
 	if(!($ans))
 	{
-		$content .= "<p class=\"ack\">WAIT FOR MY QUESTION BATMAN</p>";
+		$content .= "<p class=\"ack\">WAIT FOR MY QUESTION SHERLOCK</p>";
 	}
 
 
@@ -105,28 +107,28 @@ if (!isset($_SESSION["fbuid"]))
 		  $content = "<img src=\"imgs/levels/winmob.jpg\"><p class=\"ack\">winner<br>";
 		else
 		$content = "<img src=\"imgs/correct/cr".rand(1,7).".jpg\"><p class=\"ack\">RIGHT ANSWER<br> <a href = \"index.php\">Next Level</a></p>";
-		$sql = "UPDATE users SET level = '" . $level . "' WHERE fbuid = '" . $_SESSION["fbuid"] ."'"; 
+		$sql = "UPDATE users SET level = '" . $level . "' WHERE fbuid = '" . $_SESSION["fbuid"] ."'";
 		$ref = $result->query($sql);
 
-		
+
 	}
 	else
 		{
-		
+
 		$content = "<img class=\"ack\" src=\"imgs/wrong/wr".rand(1,6).".jpg\"><p class=\"ack\">Wrong answer<br> <a href = \"index.php\">TRY AGAIN</a></p>";
-		
+
 
 		}
 	print $content;
-	
+
 ?>
 	<!--php code goes here-->
 				</div>
 			</div>
-			
+
 		</div>
-		
-			
+
+
 	</div>
 </body>
 </html>
