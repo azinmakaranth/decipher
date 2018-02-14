8ad6bb3d59cbb331b21233e50fe33dcc880d2177
azin.makaranth <azin.makaranth@gmail.com>
2018-02-14 15:22:47 +0530

    title.answer.php

--- answer.php
+++ answer.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION["fbuid"]))
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Unravel</title>
+	<title>Decipher|2018</title>
 	<link rel="icon" href="../images/icon.png" type="image/png" sizes="16x16">
 	<link rel="stylesheet" href="css/style.css" type="text/css">
 	<link rel="stylesheet" type="text/css" href="css/mobile.css">
