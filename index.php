41a23e2c728eb40d24053022c059feca073c8b14
azin.makaranth <azin.makaranth@gmail.com>
2018-02-18 20:43:04 +0530

    footer

--- index.php
+++ index.php
@@ -234,6 +234,19 @@ echo "</div>
 
 
 	</div>
+	<style>
+.footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    color: white;
+    text-align: center;
+}
+</style>
+	<div class="footer">
+  <p>Footer</p>
+</div>
 </body>
 </html>
 ";
